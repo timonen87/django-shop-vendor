3546fd781bce0ed0431efa28003e70a81678dba5
timonen87 <timonen.as@gmail.com>
2023-03-29 11:10:54 +0300

    start build

--- Bank/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Bank
-
-
-class BankAdmin(admin.ModelAdmin):
-    list_display = ('id','bank_name', 'slug', 'created', 'updated',)
-
-admin.site.register(Bank, BankAdmin)
--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Bank.urls')),
+    
 ]
 
 if settings.DEBUG:
